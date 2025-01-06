82644c6322e7e0d42e6f2e4f5c3b102a415de7a9
Ajiro <adilismail7654321@gmail.com>
2025-01-06 18:18:45 +0500

    4TH commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const router = express.Router();
 
-// Route to test the server
-router.get('/', (req, res) => {
-  res.send('Server is running!');
+const Reader = require('@maxmind/geoip2-node').Reader;
+Reader.open('a.mmdb').then(reader => {
+  const response = reader.city('111.88.27.29');
+  console.log(response); // 'US'
+//   console.log(response.city.names.en); // 'Minneapolis'
+//   console.log(response.postal.code); // '55407'
 });
-
-module.exports = router;
-
 
