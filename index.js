d1fe548b466e1c6089288feed2977c34bd8ef7c0
Ajiro <adilismail7654321@gmail.com>
2025-01-07 19:46:39 +0500

    express js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const Reader = require('@maxmind/geoip2-node').Reader;
-Reader.open('a.mmdb').then(reader => {
+Reader.open('city.mmdb').then(reader => {
   const response = reader.city('111.88.27.29');
   console.log(response); // 'US'
 //   console.log(response.city.names.en); // 'Minneapolis'
